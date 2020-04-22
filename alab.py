38d119becdcbf5ec526c1d10f03f254eb6cbeded
XuPeng-SH <xupeng3112@163.com>
2020-04-22 22:47:23 +0800

    add data.py

--- alab.py
+++ alab.py
@@ -2,6 +2,7 @@ import sys
 import random
 import time
 from collections import defaultdict
+import threading
 from init_db import db
 
 SQLALCHEMY_DATABASE_URI='sqlite:////tmp/meta_lab/meta.sqlite?check_same_thread=False'
@@ -42,9 +43,43 @@ STRING_FILE = 2
 IVFSQ8_FILE = 3
 FILE_TYPES = [BINARY_FILE, STRING_FILE, IVFSQ8_FILE]
 
-def create_snapshot(new_files, prev=None):
+class Head: pass
+
+class SnapshotsProxy:
+    def __init__(self, node, prev=None):
+        self.node = node
+        self._prev = prev
+
+    @property
+    def prev(self):
+        if isinstance(self._prev, Head):
+            return None
+        if self._prev is not None:
+            return self._prev
+
+        self._prev = db.Session.query(CollectionSnapshots).filter(
+                CollectionSnapshots.id < self.node.id).order_by(CollectionSnapshots.id.desc()).first()
+        if self._prev is None:
+            self._prev = Head()
+            return None
+
+        return self._prev
+
+class SnapshotsList:
+    def __init__(self, from_db=True):
+        self.nodes = []
+        if from_db:
+            self.nodes = db.Session.query(CollectionSnapshots).order_by(CollectionSnapshots.id
+                    ).all()
+
+    @property
+    def current(self):
+        return self.nodes[-1]
+
+
+def create_snapshot(new_files, segment=None, prev=None):
     resources = []
-    segment = collection.create_segment()
+    segment = segment if segment else collection.create_segment()
     resources.append(segment)
     for i in range(new_files):
         f = segment.create_file(ftype=random.choice(FILE_TYPES), lsn=get_lsn())
@@ -55,8 +90,6 @@ def create_snapshot(new_files, prev=None):
     segment_commit = segment.commit_files(*resources[1:])
 
     Commit(segment_commit)
-    # snapshot = collection.create_ss()
-    # snapshot.append_mappings(segment_commit)
     snapshot = segment_commit.commit_snapshot()
 
     if prev:
@@ -66,12 +99,134 @@ def create_snapshot(new_files, prev=None):
     return snapshot
 
 
+import queue
+class Woker(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.q = queue.Queue()
+        self.stale_snapshot = set()
+
+    def handle_event(self, event):
+        print(f'Handling event {event}')
+        self.stale_snapshot.add(event)
+
+        print(f'{event.stale_commit_ids}')
+        # print(f'NEW {event.new_commits}')
+        # Commit(to_delete=[event])
+
+    def run(self):
+        while True:
+            event = self.q.get()
+            print(f'event  {event}')
+            if not event:
+                break
+            self.handle_event(event)
+
+    def stop(self):
+        self.q.put(False)
+        self.join()
+
+    def submit(self, event):
+        self.q.put(event)
+
+class WorkerContext:
+    def __init__(self, current, prev=None):
+        self.curr_id = current.id
+        self.curr_mappings = set(current.mappings)
+        self.prev_id = prev.id if prev else None
+        self.prev_mappings = set(prev.mappings) if prev else set()
+        self.stale_commit_ids = self.prev_mappings - self.curr_mappings
+        self.new_commit_ids = self.curr_mappings - self.prev_mappings
+        self._stale_files = None
+        self._stale_commits = None
+        self._new_commits = None
+
+    @property
+    def new_commits(self):
+        if self._new_commits is not None:
+            return self._new_commits
+        if not self.new_commit_ids:
+            self._new_commits = []
+            return self._new_commits
+
+        self._new_commits = db.Session.query(SegmentCommits).filter(
+                SegmentCommits.id.in_(self.new_commit_ids)
+                ).order_by(SegmentCommits.segment_id).all()
+        return self._new_commits
+
+    @property
+    def stale_commits(self):
+        if self._stale_commits is not None:
+            return self._stale_commits
+        if not self.stale_commit_ids:
+            self._stale_commits = []
+            return self._stale_commits
+
+        self._stale_commits = db.Session.query(SegmentCommits).filter(
+                SegmentCommits.id.in_(self.stale_commit_ids)
+                ).order_by(SegmentCommits.segment_id).all()
+        return self._stale_commits
+
+    @property
+    def stale_files(self):
+        if self._stale_files is not None:
+            return self._stale_files
+
+        if not self.stale_commits:
+            self._stale_files = []
+            return self._stale_files
+        stale_related_segment_ids = set([commit.segment_id for commit in self.stale_commits])
+        new_related_segment_ids = set([commit.segment_id for commit in self.new_commits])
+        stale_segments = stale_related_segment_ids - new_related_segment_ids
+        common_segments = stale_related_segment_ids & new_related_segment_ids
+
+        if len(stale_segments) == 0 and len(common_segments) == 0:
+            self._stale_files = []
+            return self._stale_files
+
+        cursor = 0
+        prev_common_commits = []
+        for commit in self.stale_commits:
+            for segment_id in common_segments[cursor:]:
+                cursor = segment_id
+                if commit.segment_id == segment_id:
+                    prev_common_commits.append(commit)
+                    break
+
+        curr_comm_commits = []
+        for commit in self.new_commits:
+            for segment_id in common_segments[cursor:]:
+                cursor = segment_id
+                if commit.segment_id == segment_id:
+                    curr_comm_commits.append(commit)
+                    break
+
+
+        prev_possible_stale_files = []
+        for commit in prev_common_commits:
+            prev_common_commits.extend(commit.files.all())
+
+        curr_files = []
+        for commit in curr_comm_commits:
+            curr_files.extend(commit.files.all())
+
+        diff_stale_files = set(prev_common_commits) - set(curr_files)
+
+
+        # db.Session.query(SegmentCommits)..filter().filter(SegmentCommits.segment_id.in_(stale_related_segment_ids))
+
+        return self._stale_files
+
+
 VECTOR_FIELD = 1
 INT_FIELD = 2
 STRING_FIELD = 3
 
 IVFSQ8 = 1
 IVFFLAT = 2
+
+worker = Woker()
+worker.start()
 
 collection = Collections()
 vf = collection.create_field(name='vector', ftype=VECTOR_FIELD, params={'dimension': 512})
@@ -81,25 +236,44 @@ Commit(collection, vf, vfi, idf)
 print(f'fields: {[ (f.name, f.params) for f in collection.fields.all()]}')
 
 prev = None
-for _ in range(10):
+for _ in range(2):
     start = time.time()
     snapshot = create_snapshot(2, prev=prev)
-    Commit(snapshot, to_delete=[prev])
+    Commit(snapshot)
+    context = WorkerContext(snapshot, prev)
+    prev and worker.submit(context)
+
     end = time.time()
     print(f'Takes {end-start}')
     prev = snapshot
 
+commit = prev.commits.first()
+ss = create_snapshot(3, segment=commit.segment, prev=prev)
+Commit(ss)
+context = WorkerContext(ss, prev)
+print(f'XXXXXX {context.stale_files}')
+prev and worker.submit(context)
+
 # print(f'Snapshots {collection.snapshots.all()}')
+time.sleep(0.1)
 snapshots = collection.snapshots.all()
 for ss in snapshots:
     print(f'{ss} {[f.id for f in ss.commits.all()]}')
+    p = SnapshotsProxy(ss)
+    prev = p.prev
+    print(f'Snapshots {ss.id} prev={prev.id if prev else None}')
 
-segments = collection.segments.all()
-for segment in segments:
+smgr = SnapshotsList()
+print(f'Current {smgr.current.id}')
+
+# segments = collection.segments.all()
+# for segment in segments:
 #     f = segment.create_file(lsn=get_lsn())
 #     Commit(f)
 #     c = segment.commit_files(f)
 #     Commit(c)
-    print(f'{segment} {segment.commits.all()}')
+    # print(f'{segment} {segment.commits.all()}')
 
+# time.sleep(0.1)
+worker.stop()
 sys.exit(0)
---
+++ data.py
@@ -0,0 +1,163 @@
+from init_db import db
+SQLALCHEMY_DATABASE_URI='sqlite:////tmp/meta_lab/meta.sqlite?check_same_thread=False'
+db.init_db(uri=SQLALCHEMY_DATABASE_URI)
+
+from collections import defaultdict, OrderedDict
+from models import Segments, SegmentCommits, Collections, CollectionSnapshots
+
+class Proxy:
+    def __init__(self, node, cleanup=None):
+        self.node = node
+        self.prev = None
+        self.next = None
+        self.refcnt = 0
+        self.cleanupcb = cleanup
+
+    def set_next(self, next_node):
+        next_node.prev = self
+        self.next = next_node
+        if self.refcnt == 0:
+            self.cleanup()
+        return self
+
+    def set_prev(self, prev_node):
+        prev_node.next = self
+        self.prev = prev_node
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        return getattr(self.node, attr)
+
+    def is_tail(self):
+        return self.next == None
+
+    def is_head(self):
+        return self.prev == None
+
+    def ref(self):
+        self.refcnt += 1
+
+    def unref(self):
+        self.refcnt -= 1
+        if self.refcnt <= 0:
+            self.cleanup()
+
+    def cleanup(self):
+        self.cleanupcb and self.cleanupcb(self.node)
+        self.do_cleanup()
+
+    def do_cleanup(self):
+        pass
+
+
+class DBProxy(Proxy):
+    def __init__(self, node, cleanup=None):
+        super().__init__(node, cleanup=cleanup)
+        self.model = node.__class__
+
+    def do_cleanup(self):
+        print(f'Doing CLEANUP {self.model}')
+        db.Session.delete(self.node)
+        db.Session.commit()
+
+
+class SnapshotsMgr:
+    def __init__(self):
+        self.all_snapshots = defaultdict(OrderedDict)
+        self.heads = {}
+        self.tails = {}
+        self.keeps = 1
+
+    def load_snapshots(self, collection):
+        cid = collection
+        if isinstance(collection, Collections):
+            cid = collection.id
+
+        snapshots = db.Session.query(CollectionSnapshots).filter(CollectionSnapshots.collection_id==cid
+                ).order_by(CollectionSnapshots.id.desc()).all()
+
+        next_node = None
+        num = 0
+        for ss in snapshots:
+            proxy = DBProxy(ss)
+            num += 1
+            if num > self.keeps:
+                proxy.unref()
+                continue
+
+            proxy.ref()
+            if next_node is not None:
+                proxy.set_next(next_node)
+            else:
+                self.tails[cid] = proxy
+            self.all_snapshots[cid][ss.id] = proxy
+            next_node = proxy
+
+        if next_node is not None:
+            self.heads[cid] = next_node
+
+        print(f'{self.all_snapshots}')
+
+    def close_snapshots(self, collection):
+        cid = collection
+        if isinstance(collection, Collections):
+            cid = collection.id
+
+        self.all_snapshots.pop(cid)
+        # import pdb;pdb.set_trace()
+        self.heads.pop(cid, None)
+        self.tails.pop(cid, None)
+
+    def get_snapshot(self, collection, snapshot_id=None):
+        cid = collection
+        if isinstance(cid, Collections):
+            cid = cid.id
+        collection_sss = self.all_snapshots.get(cid, None)
+        if not collection_sss:
+            self.load_snapshots(cid)
+            collection_sss = self.all_snapshots.get(cid, None)
+            if not collection_sss:
+                return None
+        if not snapshot_id:
+            return self.tails[cid]
+
+        return collection_sss.get(snapshot_id, None)
+
+    def drop_head(self, collection):
+        cid = collection
+        if isinstance(cid, Collections):
+            cid = cid.id
+        head = self.heads.pop(cid, None)
+        if not head:
+            return
+        self.all_snapshots[cid].pop(head.id, None)
+        next_node = head.next
+        if not next_node:
+            return
+        next_node.prev = None
+        self.heads[cid] = next_node
+
+        head.unref()
+
+
+class SegmentCommitsMgr:
+    def __init__(self):
+        self.all_commits = {}
+
+    def load_commits(self, segment):
+        sid = segment
+        if isinstance(segment, Segments):
+            sid = segment.id
+
+        commits = db.Session.query(SegmentCommits).filter(SegmentCommits.segment_id==sid).all()
+        for commit in commits:
+            self.all_commits[commit.id] = commit
+
+if __name__ == '__main__':
+    collection = db.Session.query(Collections).first()
+    ss_mgr = SnapshotsMgr()
+    ss_mgr.load_snapshots(collection)
+    ss = ss_mgr.get_snapshot(collection)
+    ss_mgr.drop_head(collection)
+    ss_mgr.close_snapshots(collection)
