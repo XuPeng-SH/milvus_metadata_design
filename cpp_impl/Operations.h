6a38049bdb2b77f55db35e856e360f56e0b8d46e
XuPeng-SH <xupeng3112@163.com>
2020-05-15 18:25:44 +0800

    Update 6

--- cpp_impl/Operations.h
+++ cpp_impl/Operations.h
@@ -19,10 +19,9 @@ using StepsT = std::vector<std::any>;
 /*                                   Segment::VecT, */
 /*                                   SegmentFile::VecT>; */
 
-template <typename Derived>
 class Operations {
 public:
-    static constexpr const char* Name = Derived::Name;
+    /* static constexpr const char* Name = Derived::Name; */
     Operations(ScopedSnapshotT prev_ss);
     Operations(ID_TYPE collection_id, ID_TYPE commit_id = 0);
 
@@ -42,35 +41,31 @@ protected:
     std::vector<ID_TYPE> ids_;
 };
 
-template <typename Derived>
-Operations<Derived>::Operations(ScopedSnapshotT prev_ss) : prev_ss_(prev_ss) {}
+Operations::Operations(ScopedSnapshotT prev_ss) : prev_ss_(prev_ss) {}
 
-template <typename Derived>
-Operations<Derived>::Operations(ID_TYPE collection_id, ID_TYPE commit_id) :
+Operations::Operations(ID_TYPE collection_id, ID_TYPE commit_id) :
     prev_ss_(Snapshots::GetInstance().GetSnapshot(collection_id, commit_id)) {
 }
 
-template <typename Derived>
 template<typename StepT>
 void
-Operations<Derived>::AddStep(const StepT& step) {
+Operations::AddStep(const StepT& step) {
     steps_.push_back(std::make_shared<StepT>(step));
     /* auto& container = std::get<Index<typename StepT::VecT, StepsT>::value>(steps_); */
     /* container.push_back(std::make_shared<StepT>(step)); */
 }
 
-template <typename Derived>
 void
-Operations<Derived>::OnExecute() {
-    std::cout << "Operations " << Name << " is OnExecute with " << steps_.size() << " steps" << std::endl;
+Operations::OnExecute() {
+    /* std::cout << "Operations " << Name << " is OnExecute with " << steps_.size() << " steps" << std::endl; */
     auto& store = Store::GetInstance();
     store.DoCommitOperation(*this);
 }
 
-class BuildOperation : public Operations<BuildOperation> {
+class BuildOperation : public Operations {
 public:
     static constexpr const char* Name = "Build";
-    using BaseT = Operations<BuildOperation>;
+    using BaseT = Operations;
 
     BuildOperation(ScopedSnapshotT prev_ss) : BaseT(prev_ss) {};
     BuildOperation(ID_TYPE collection_id, ID_TYPE commit_id = 0) : BaseT(collection_id, commit_id) {};
