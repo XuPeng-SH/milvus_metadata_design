9c11e56cc37ee54755fde1a992d68255c069cae8
XuPeng-SH <xupeng3112@163.com>
2020-05-17 21:07:20 +0800

    Update Operations 19

--- cpp_impl/Operations.h
+++ cpp_impl/Operations.h
@@ -367,10 +367,21 @@ public:
     NewSegmentOperation(const OperationContext& context, ID_TYPE collection_id, ID_TYPE commit_id = 0)
         : BaseT(context, collection_id, commit_id) {};
 
-    /* bool DoExecute() override; */
+    bool DoExecute() override {
+        auto i = 0;
+        for(; i<context_.new_segment_files.size(); ++i) {
+            std::any_cast<SegmentFilePtr>(steps_[i])->Activate();
+        }
+        std::any_cast<SegmentCommitPtr>(steps_[i++])->Activate();
+        std::any_cast<PartitionCommitPtr>(steps_[i++])->Activate();
+        std::any_cast<CollectionCommitPtr>(steps_[i++])->Activate();
+        return true;
+    }
+
     bool PreExecute() override {
         // PXU TODO:
         // 1. Check all requried field elements have related segment files
+        // 2. Check Stale and others
         SegmentCommitOperation op(context_, prev_ss_);
         op.OnExecute();
         context_.new_segment_commit = op.GetResource();
@@ -379,16 +390,18 @@ public:
         PartitionCommitOperation pc_op(context_, prev_ss_);
         pc_op.OnExecute();
 
-        /* OperationContext cc_context; */
-        /* cc_context.new_partition_commit = pc_op.GetResource(); */
-        /* CollectionCommitOperation cc_op(cc_context, prev_ss_); */
-        /* cc_op.OnExecute(); */
+        OperationContext cc_context;
+        cc_context.new_partition_commit = pc_op.GetResource();
+        CollectionCommitOperation cc_op(cc_context, prev_ss_);
+        cc_op.OnExecute();
 
-        /* AddStep(*context_.new_segment_file); */
-        /* AddStep(*context_.new_segment_commit); */
-        /* AddStep(*pc_op.GetResource()); */
-        /* AddStep(*cc_op.GetResource()); */
-
+        for (auto& new_segment_file : context_.new_segment_files) {
+            AddStep(*new_segment_file);
+        }
+        AddStep(*context_.new_segment_commit);
+        AddStep(*pc_op.GetResource());
+        AddStep(*cc_op.GetResource());
+        return true;
     }
 
     SegmentPtr NewSegment() {
