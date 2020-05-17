73f14b03d675897cdbfa0048cb54cfdbd4d71d39
XuPeng-SH <xupeng3112@163.com>
2020-05-17 22:24:36 +0800

    Refactor Snapshots 4

--- cpp_impl/Snapshots.h
+++ cpp_impl/Snapshots.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Snapshot.h"
+#include "SnapshotHolder.h"
 #include "Resources.h"
 #include "Holders.h"
 #include "Store.h"
@@ -18,146 +18,6 @@
 #include <atomic>
 #include <chrono>
 
-
-class SnapshotsHolder {
-public:
-    using ScopedPtr = std::shared_ptr<ScopedSnapshotT>;
-
-    SnapshotsHolder(ID_TYPE collection_id, GCHandler gc_handler = nullptr, size_t num_versions = 1)
-        : collection_id_(collection_id), num_versions_(num_versions), gc_handler_(gc_handler), done_(false) {}
-
-    ID_TYPE GetID() const { return collection_id_; }
-    bool Add(ID_TYPE id) {
-        {
-            std::unique_lock<std::mutex> lock(mutex_);
-            if (active_.size() > 0 && id < max_id_) {
-                return false;
-            }
-        }
-        Snapshot::Ptr oldest_ss;
-        {
-            auto ss = std::make_shared<Snapshot>(id);
-
-            std::unique_lock<std::mutex> lock(mutex_);
-            if (done_) { return false; };
-            ss->RegisterOnNoRefCB(std::bind(&Snapshot::UnRefAll, ss));
-            ss->Ref();
-            auto it = active_.find(id);
-            if (it != active_.end()) {
-                return false;
-            }
-
-            if (min_id_ > id) {
-                min_id_ = id;
-            }
-
-            if (max_id_ < id) {
-                max_id_ = id;
-            }
-
-            active_[id] = ss;
-            if (active_.size() <= num_versions_)
-                return true;
-
-            auto oldest_it = active_.find(min_id_);
-            oldest_ss = oldest_it->second;
-            active_.erase(oldest_it);
-            min_id_ = active_.begin()->first;
-        }
-        ReadyForRelease(oldest_ss); // TODO: Use different mutex
-        return true;
-    }
-
-    void BackgroundGC();
-
-    void NotifyDone();
-
-    ScopedSnapshotT GetSnapshot(ID_TYPE id = 0, bool scoped = true);
-
-    void GCHandlerTestCallBack(Snapshot::Ptr ss) {
-        std::unique_lock<std::mutex> lock(gcmutex_);
-        to_release_.push_back(ss);
-        lock.unlock();
-        cv_.notify_one();
-    }
-
-    bool SetGCHandler(GCHandler gc_handler) {
-        gc_handler_ = gc_handler;
-    }
-
-private:
-    void ReadyForRelease(Snapshot::Ptr ss) {
-        if (gc_handler_) {
-            gc_handler_(ss);
-        }
-    }
-
-    std::mutex mutex_;
-    std::mutex gcmutex_;
-    std::condition_variable cv_;
-    ID_TYPE collection_id_;
-    ID_TYPE min_id_ = std::numeric_limits<ID_TYPE>::max();
-    ID_TYPE max_id_ = std::numeric_limits<ID_TYPE>::min();
-    std::map<ID_TYPE, Snapshot::Ptr> active_;
-    std::vector<Snapshot::Ptr> to_release_;
-    size_t num_versions_ = 1;
-    GCHandler gc_handler_;
-    std::atomic<bool> done_;
-};
-
-ScopedSnapshotT
-SnapshotsHolder::GetSnapshot(ID_TYPE id, bool scoped) {
-    std::unique_lock<std::mutex> lock(mutex_);
-    /* std::cout << "Holder " << collection_id_ << " actives num=" << active_.size() */
-    /*     << " latest=" << active_[max_id_]->GetID() << " RefCnt=" << active_[max_id_]->RefCnt() <<  std::endl; */
-    if (id == 0 || id == max_id_) {
-        auto ss = active_[max_id_];
-        return ScopedSnapshotT(ss, scoped);
-    }
-    if (id < min_id_ || id > max_id_) {
-        return ScopedSnapshotT();
-    }
-
-    auto it = active_.find(id);
-    if (it == active_.end()) {
-        return ScopedSnapshotT();
-    }
-    return ScopedSnapshotT(it->second, scoped);
-}
-
-void
-SnapshotsHolder::NotifyDone() {
-    std::unique_lock<std::mutex> lock(gcmutex_);
-    done_ = true;
-    cv_.notify_all();
-}
-
-void
-SnapshotsHolder::BackgroundGC() {
-    while (true) {
-        if (done_.load(std::memory_order_acquire)) {
-            break;
-        }
-        std::vector<Snapshot::Ptr> sss;
-        {
-            std::unique_lock<std::mutex> lock(gcmutex_);
-            cv_.wait_for(lock, std::chrono::milliseconds(100), [this]() {return to_release_.size() > 0;});
-            if (to_release_.size() > 0) {
-                /* std::cout << "size = " << to_release_.size() << std::endl; */
-                sss = to_release_;
-                to_release_.clear();
-            }
-        }
-
-        for (auto& ss : sss) {
-            ss->UnRef();
-            /* std::cout << "BG Handling " << ss->GetID() << " RefCnt=" << ss->RefCnt() << std::endl; */
-        }
-
-    }
-}
-
-using SnapshotsHolderPtr = std::shared_ptr<SnapshotsHolder>;
 
 class Snapshots {
 public:
@@ -265,7 +125,7 @@ Snapshots::LoadNoLock(ID_TYPE collection_id) {
     if (collection_commit_ids.size() == 0) {
         return nullptr;
     }
-    auto holder = std::make_shared<SnapshotsHolder>(collection_id,
+    auto holder = std::make_shared<SnapshotHolder>(collection_id,
             std::bind(&Snapshots::SnapshotGCCallback, this, std::placeholders::_1));
     for (auto c_c_id : collection_commit_ids) {
         holder->Add(c_c_id);
