24c3006644166f4e665244311588b09e09d2399b
peng.xu <peng.xu@zilliz.com>
2020-05-18 13:43:27 +0800

    (meta_lab): Refactor CompoundOperations

--- cpp_impl/CompoundOperations.cpp
+++ cpp_impl/CompoundOperations.cpp
@@ -55,3 +55,69 @@ BuildOperation::DoExecute() {
     std::any_cast<CollectionCommitPtr>(steps_[3])->Activate();
     return true;
 }
+
+SegmentFilePtr
+BuildOperation::NewSegmentFile(const SegmentFileContext& context) {
+    SegmentFileOperation new_sf_op(context, prev_ss_);
+    new_sf_op.OnExecute();
+    context_.new_segment_files.push_back(new_sf_op.GetResource());
+    return new_sf_op.GetResource();
+}
+
+bool
+NewSegmentOperation::DoExecute() {
+    auto i = 0;
+    for(; i<context_.new_segment_files.size(); ++i) {
+        std::any_cast<SegmentFilePtr>(steps_[i])->Activate();
+    }
+    std::any_cast<SegmentCommitPtr>(steps_[i++])->Activate();
+    std::any_cast<PartitionCommitPtr>(steps_[i++])->Activate();
+    std::any_cast<CollectionCommitPtr>(steps_[i++])->Activate();
+    return true;
+}
+
+bool
+NewSegmentOperation::PreExecute() {
+    // PXU TODO:
+    // 1. Check all requried field elements have related segment files
+    // 2. Check Stale and others
+    SegmentCommitOperation op(context_, prev_ss_);
+    op.OnExecute();
+    context_.new_segment_commit = op.GetResource();
+    if (!context_.new_segment_commit) return false;
+
+    PartitionCommitOperation pc_op(context_, prev_ss_);
+    pc_op.OnExecute();
+
+    OperationContext cc_context;
+    cc_context.new_partition_commit = pc_op.GetResource();
+    CollectionCommitOperation cc_op(cc_context, prev_ss_);
+    cc_op.OnExecute();
+
+    for (auto& new_segment_file : context_.new_segment_files) {
+        AddStep(*new_segment_file);
+    }
+    AddStep(*context_.new_segment_commit);
+    AddStep(*pc_op.GetResource());
+    AddStep(*cc_op.GetResource());
+    return true;
+}
+
+SegmentPtr
+NewSegmentOperation::NewSegment() {
+    SegmentOperation op(context_, prev_ss_);
+    op.OnExecute();
+    context_.new_segment = op.GetResource();
+    return context_.new_segment;
+}
+
+SegmentFilePtr
+NewSegmentOperation::NewSegmentFile(const SegmentFileContext& context) {
+    auto c = context;
+    c.segment_id = context_.new_segment->GetID();
+    c.partition_id = context_.new_segment->GetPartitionId();
+    SegmentFileOperation new_sf_op(c, prev_ss_);
+    new_sf_op.OnExecute();
+    context_.new_segment_files.push_back(new_sf_op.GetResource());
+    return new_sf_op.GetResource();
+}
